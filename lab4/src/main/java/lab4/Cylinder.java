bdaf3938701c925fc36d04d82651de37dd29da83
NLMK1103 <Ngleminhkhoa@gmail.com>
2023-03-07 01:38:53 +0700

    lan3

---
+++ lab4/src/main/java/lab4/Cylinder.java
@@ -0,0 +1,17 @@
+package lab4;
+
+public class Cylinder extends Circle{
+    public double height;
+
+    public double getHeight(){
+        return this.height;
+    }
+
+    public double setHeight(double height){
+        return this.height = height;
+    }
+
+    public double getVolume(){
+        return super.getArea() * getHeight();
+    }
+}
